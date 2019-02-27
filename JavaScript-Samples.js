77482a294c54cc25690eb8b703f5a4627ea3c321
Kumaravel Shanmugam <M1050211@mindtree.com>
2019-02-27 10:58:16 +0530

    call & Apply & Bind

--- JavaScript-Samples.js
+++ JavaScript-Samples.js
@@ -804,3 +804,60 @@ const circle = new Circle(1);
 console.log(circle);  //outPut: {radius: 1, draw: ƒ}
 
 //  ###################### JavaScript Constructor Function End ##################
+
+
+
+
+
+//  ###################### Apply vs. Call vs. Bind Examples #####################
+// https://stackoverflow.com/questions/15455009/javascript-call-apply-vs-bind
+
+Answer in SIMPLEST form
+
+Call invokes the function and allows you to pass in arguments one by one.
+Apply invokes the function and allows you to pass in arguments as an array.
+Bind returns a new function, allowing you to pass in a this array and any number of arguments.
+
+
+// Call  ###################################################
+var person1 = {firstName: 'Jon', lastName: 'Kuperman'};
+var person2 = {firstName: 'Kelly', lastName: 'King'};
+
+function say(greeting) {
+    console.log(greeting + ' ' + this.firstName + ' ' + this.lastName);
+}
+
+say.call(person1, 'Hello'); // Hello Jon Kuperman
+say.call(person2, 'Hello'); // Hello Kelly King
+
+
+
+// Apply  ##################################################
+var person1 = {firstName: 'Jon', lastName: 'Kuperman'};
+var person2 = {firstName: 'Kelly', lastName: 'King'};
+
+function say(greeting) {
+    console.log(greeting + ' ' + this.firstName + ' ' + this.lastName);
+}
+
+say.apply(person1, ['Hello']); // Hello Jon Kuperman
+say.apply(person2, ['Hello']); // Hello Kelly King
+
+
+
+// Bind ####################################################
+var person1 = {firstName: 'Jon', lastName: 'Kuperman'};
+var person2 = {firstName: 'Kelly', lastName: 'King'};
+
+function say() {
+    console.log('Hello ' + this.firstName + ' ' + this.lastName);
+}
+
+var sayHelloJon = say.bind(person1);
+var sayHelloKelly = say.bind(person2);
+
+sayHelloJon(); // Hello Jon Kuperman
+sayHelloKelly(); // Hello Kelly King
+
+
+//  ###################### Apply vs. Call vs. Bind Examples End #################
